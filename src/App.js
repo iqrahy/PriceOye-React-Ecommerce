32b691a34f11c961d2f4cf2e59a4e56723a3a389
Iqra <iqrahyderxwave@gmail.com>
2024-12-12 13:06:57 +0500

    Initialize project with clean setup and required packages

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './App.css';
+import { faFaceAngry } from '@fortawesome/free-regular-svg-icons';
+import { faCake, faCartShopping } from '@fortawesome/free-solid-svg-icons';
+
+function App() {
+  return (
+   <>
+   <h1 className="text-3xl font-bold underline text-center text-cyan-400">React app</h1>
+  
+   
+   </>
+  );
+}
+
+export default App;
+
+// tag for FontAwesome icons
+ {/* <FontAwesomeIcon className='text-red-900' icon={faFaceAngry}/> */}
