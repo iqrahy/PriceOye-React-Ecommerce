c365d7998bcc0dc522dad60c5f4762ac98f39109
Iqra <iqrahyderxwave@gmail.com>
2024-12-25 17:56:24 +0500

    work on checkout page functionality and design

--- src/components/checkout-form/CheckoutForm.jsx
+++ src/components/checkout-form/CheckoutForm.jsx
@@ -1,11 +1,13 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import Stepper from './stepper/CheckoutStepper'
+import OrderDetailsForm from './order-details-form/OrderDetailsForm'
 
 const CheckoutForm = () => {
   return (
-    <Box className="pt-32 pb-16 !bg-slate-100" >
+    <Box className="pt-32 pb-16 px-3 lg:px-8 !bg-slate-100" >
      <Stepper/>
+     <OrderDetailsForm/>
     </Box>
   )
 }
--- src/components/checkout-form/stepper/CheckoutStepper.jsx
+++ src/components/checkout-form/stepper/CheckoutStepper.jsx
@@ -1,29 +1,47 @@
-import { Box } from '@mui/material';
-import React from 'react'
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
+import { Box } from "@mui/material";
+import React from "react";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
 
-const steps = [
-  'Select master blaster campaign settings',
-  'Create an ad group',
-  'Create an ad',
-];
+const steps = ["OTP Verification", "Contact Info", "Delivery", "Payment"];
 
 const CheckoutStepper = () => {
+  const activeStep = 3;
   return (
     <>
-        <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={1} alternativeLabel>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-    </Box>
+      <Box className="md:w-[70%]">
+        <Stepper activeStep={activeStep} alternativeLabel>
+          {steps.map((label, index) => (
+            <Step key={label} completed={index < activeStep}>
+              <StepLabel
+                sx={{
+                  "& .MuiStepLabel-label": {
+                    color:
+                      index < activeStep
+                        ? "#48AFFF" 
+                        : index === activeStep
+                        ? "gray"
+                        : "gray", 
+                  },
+                  "& .MuiStepIcon-root": {
+                    color:
+                      index < activeStep
+                        ? "#48AFFF" 
+                        : index === activeStep
+                        ? "gray" 
+                        : "gray", 
+                  },
+                }}
+              >
+                {label}
+              </StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+      </Box>
     </>
-  )
-}
+  );
+};
 
-export default CheckoutStepper
+export default CheckoutStepper;
